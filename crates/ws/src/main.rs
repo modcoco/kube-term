dfbf1830261ec0acbb7bfef56db895724f2d430a
mahongqin <codepapercut@gmail.com>
2024-05-25 00:43:40 +0800

    Use struct

--- crates/ws/src/main.rs
+++ crates/ws/src/main.rs
@@ -103,13 +103,77 @@ impl ServerCertVerifier for IgnoreAllCertificateSecurity {
     }
 }
 
+#[derive(Debug)]
+struct UrlComponents {
+    domain: String,
+    port: String,
+    path: String,
+}
+
+impl UrlComponents {
+    fn format(&self) -> String {
+        format!("wss://{}:{}{}", self.domain, self.port, self.path)
+    }
+}
+
+#[derive(Debug)]
+struct QueryParamsK8sTerm {
+    container: String,
+    stdin: bool,
+    stdout: bool,
+    stderr: bool,
+    tty: bool,
+    command: String,
+    pretty: bool,
+    follow: bool,
+}
+
+impl QueryParamsK8sTerm {
+    fn format(&self) -> String {
+        format!(
+            "?container={}&stdin={}&stdout={}&stderr={}&tty={}&command={}&pretty={}&follow={}",
+            self.container,
+            self.stdin,
+            self.stdout,
+            self.stderr,
+            self.tty,
+            self.command,
+            self.pretty,
+            self.follow
+        )
+    }
+}
+
 async fn connect() -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>> {
     tracing::debug!("attempting connection");
+
+    let url_components = UrlComponents {
+        domain: "192.168.2.4".to_owned(),
+        port: "6443".to_owned(),
+        path: "/api/v1/namespaces/ns/pods/podename/".to_string(),
+    };
+
+    let query_params = QueryParamsK8sTerm {
+        container: "custom".to_string(),
+        stdin: true,
+        stdout: true,
+        stderr: true,
+        tty: true,
+        command: "ls".to_string(),
+        pretty: true,
+        follow: true,
+    };
+
+    let websocket_url = format!("{}{}", url_components.format(), query_params.format());
+
     let request = Request::builder()
-        .uri("wss://192.168.2.4:6443/api/v1/namespaces/ns/pods/podename/exec?container=custom&stdin=true&stdout=true&stderr=true&tty=true&command=ls&pretty=true&follow=true")
+        .uri(websocket_url)
         .header(HOST, "192.168.2.4:6443")
         .header("Origin", "https://192.168.2.4:6443")
-        .header(SEC_WEBSOCKET_KEY, tokio_tungstenite::tungstenite::handshake::client::generate_key())
+        .header(
+            SEC_WEBSOCKET_KEY,
+            tokio_tungstenite::tungstenite::handshake::client::generate_key(),
+        )
         .header(CONNECTION, "Upgrade")
         .header(UPGRADE, "websocket")
         .header(SEC_WEBSOCKET_VERSION, "13")
